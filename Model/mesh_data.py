ac18af83413c53f86e855420c87d35a6839932a9
ckreisl <christoph.kreisl@gmx.de>
2020-03-14 19:50:40 +0100

    Added GPL license to all files

--- Model/mesh_data.py
+++ Model/mesh_data.py
@@ -1,8 +1,23 @@
+"""
+    This file is part of EMCA, an explorer of Monte-Carlo based Algorithms.
+    Copyright (c) 2019-2020 by Christoph Kreisl and others.
+    EMCA is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+    EMCA is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from Types.factory import TypeFactory
 from Types.color3 import Color3f
 import logging
 
 import numpy as np
+
 
 class Mesh(object):
 
--- Renderer/path.py
+++ Renderer/path.py
@@ -1,3 +1,17 @@
+"""
+    This file is part of EMCA, an explorer of Monte-Carlo based Algorithms.
+    Copyright (c) 2019-2020 by Christoph Kreisl and others.
+    EMCA is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+    EMCA is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from Renderer.ray import Ray
 from Renderer.vertex import Vertex
 from Renderer.intersection import Intersection
